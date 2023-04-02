1a1d8e30eef7cd04c8a90ec1a255e0ae9d9d1fec
herzfr <47446320+herzfr@users.noreply.github.com>
2023-04-02 18:30:16 +0700

    update

--- app-angular/src/app/app.module.ts
+++ app-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { APP_BASE_HREF } from '@angular/common'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app-react/src/root.component.tsx
+++ app-react/src/root.component.tsx
@@ -1,3 +1,6 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <section>{props.name} is mounted!
+    <br />
+    <a href="/angular-app">Go To Angular</a>
+  </section>;
 }
